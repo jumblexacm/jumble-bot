cb19c29b0deca3ee7b2075dd836c70eb8512d68b
Kira Hammond <kirmar@stormyriver.com>
2022-07-12 00:51:18 -0700

    Try to verify/authenticate Discord
     * Discord still accepts the URL, but based on the console logs,
       the authentication still isn't working.

---
+++ task/lambda_function.py
@@ -0,0 +1,61 @@
+# Modified from:
+# https://betterprogramming.pub/build-a-discord-bot-with-aws-lambda-api-gateway-cc1cff750292
+# https://stackoverflow.com/a/67661930
+
+import os
+import sys
+import json
+
+sys.path.append('pynacl')
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
+# Discord app's public key
+# stored as the Lambda function's environment variable
+PUBLIC_KEY = os.environ['PUBLIC_KEY']
+
+# Source: https://www.christopherbiscardi.com/validating-discord-slash-command-ed25519-signatures-in-rust
+def lambda_handler(request, lambda_context):
+    verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+    
+    signature = request['headers']['x-signature-ed25519']
+    timestamp = request['headers']['x-signature-timestamp']
+    body = request['body']
+    
+    # print("signature: " + signature)
+    # print("timestamp: " + timestamp)
+    # print("type(body): " + str(type(body)))
+    
+    try:
+        verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+        # print("valid")
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'type': 1})
+        }
+    except:
+        # print("invalid")
+        return {
+            'statusCode': 401,
+            'body': json.dumps("invalid request signature")
+        }
+
+
+"""
+# Source: https://discordpy.readthedocs.io/en/stable/quickstart.html
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+def lambda_handler(event):
+    client.run(PUBLIC_KEY)]
+"""
